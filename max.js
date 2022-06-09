9d599484e9298b9f0833fe39e01628f5395b610e
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-09 12:11:58 +0600

    thard

--- max.js
+++ max.js
@@ -1,33 +1,33 @@
-// // find max value
+// find max value
 
-// const business = 450;
-// const minister = 350;
-// if (business > minister) {
-//     console.log("business person er pola is bigger then minister");
-// }
-// else {
+const business = 450;
+const minister = 350;
+if (business > minister) {
+    console.log("business person er pola is bigger then minister");
+}
+else {
 
-//     console.log('minister er pola is better then business man ');
-// }
+    console.log('minister er pola is better then business man ');
+}
 
 
 
-// // three number variable compare ________
+// three number variable compare ________
 
-// const Business = 40;
-// const Minister = 20;
-// const army = 16;
+const Business = 40;
+const Minister = 20;
+const army = 16;
 
-// if (business > minister && business > army) {
-//     console.log('business is bigger');
-// }
-// else if (minister > business && minister > army) {
-//     console.log('minister is best');
-// }
+if (business > minister && business > army) {
+    console.log('business is bigger');
+}
+else if (minister > business && minister > army) {
+    console.log('minister is best');
+}
 
-// else {
-//     console.log('army is bigger');
-// }
+else {
+    console.log('army is bigger');
+}
 
 
 
@@ -46,7 +46,5 @@ function findlargest(first, second) {
         return second;
     }
 }
-
-
 const larger = findlargest(3, 4)
 console.log('largest is : ', larger);
