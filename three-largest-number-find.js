98b52a1ffa3c37f617d8d74f836a9065ee07a197
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-09 12:30:36 +0600

    fourth

--- three-largest-number-find.js
+++ three-largest-number-find.js
@@ -1,16 +1,23 @@
-function findThreenumber(num1, num2, num3) {
+// find the largest number three number 
 
-    if (num1 >= num2 && num1 >= num3) {
-        return num1;
-    }
-    else if (num2 >= num1 && num2 >= num3) {
-        return num2;
-    }
-    else {
-        return num3;
-    }
+// function findThreenumber(num1, num2, num3) {
+//     if (num1 >= num2 && num1 >= num3) {
+//         return num1;
+//     }
+//     else if (num2 >= num1 && num2 >= num3) {
+//         return num2;
+//     }
+//     else {
+//         return num3;
+//     }
+// }
+// const findnumberlargest = findThreenumber(120, 114, 30);
+// console.log("this is largest nuumber:", findnumberlargest);
 
-}
 
-const findnumberlargest = findThreenumber(120, 114, 30);
-console.log("this is largest nuumber:", findnumberlargest);
+
+
+
+
+
+
