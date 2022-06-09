fc78ded025e5f21670d8dc28b51caae4aa625d7b
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-09 13:36:36 +0600

    six

---
+++ largest-element-array.js
@@ -0,0 +1,23 @@
+function largestelement(numbers) {
+
+    let largest = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+
+        const element = numbers[i]
+
+        if (element > largest) {
+
+            largest = element;
+
+        }
+
+    }
+
+    return largest;
+}
+const ages = [11, 22, , 55, 44, 88, 85, 25, 38];
+
+const oldages = largestelement(ages);
+
+console.log(oldages)
