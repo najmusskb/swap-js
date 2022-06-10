a93ac00733a3c5258ff129080aeb1cbb6d135b1c
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-06-10 08:10:00 +0600

    recursive

--- recursion-advance-fibonacci-serious.js
+++ recursion-advance-fibonacci-serious.js
@@ -1,37 +1,37 @@
-// // recursion
-// /* 3rd =2nd + 1st 
+// recursion
+/* 3rd =2nd + 1st 
 
-//  4th = 3rd + 2nd 
+ 4th = 3rd + 2nd 
 
-//  5th = 4th + 5th 
+ 5th = 4th + 5th 
 
-//  119TH = 118th + 117th
+ 119TH = 118th + 117th
 
-//  nth = (n-1)th + (n-2)th
+ nth = (n-1)th + (n-2)th
 
-//  ith = (i-1)th + (i-2)
+ ith = (i-1)th + (i-2)
 
-//  */
+ */
 
-// // [0, 1,   1, 2, 3, 5, 8, 13, 21, 34, 55]
+// [0, 1,   1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 
 
-// function fibonacciii(n) {
-//     if (n == 0) {
-//         return 0;
-//     }
-//     if (n == 1) {
-//         return 1;
-//     }
+function fibonacciii(n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
 
-//     return fibonacciii(n - 1) + fibonacciii(n - 2);
+    return fibonacciii(n - 1) + fibonacciii(n - 2);
 
-// }
+}
 
-// const fiboelemen = fibonacciii(6);
+const fiboelemen = fibonacciii(6);
 
-// console.log(fiboelement);
+console.log(fiboelement);
 
 
 
